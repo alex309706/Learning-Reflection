4c34fc5b2df5374c991ef71d8852fa1beb4e64b4
alex309706 <alex309706@mail.ru>
2021-06-15 20:44:15 +0300

    Add project files.

---
+++ Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student alex = new Student();
+            Type typeOfObject = alex.GetType();
+
+            Type gettingTypeByTypeOf = typeof(Student);
+            Type gettingTypeByStaticMethod = Type.GetType("Basics.Student",false,true);
+
+            Console.WriteLine(gettingTypeByTypeOf.ToString());
+            Console.WriteLine(typeOfObject.ToString());
+            Console.WriteLine(gettingTypeByStaticMethod.ToString());
+
+        }
+
+    }
+    class Student 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string GradeBookNumber { get; set; }
+
+    }
+}
